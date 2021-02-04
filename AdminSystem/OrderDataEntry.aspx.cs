0569bce910674a302b667765c28a00db36b51902
Connor Hayden <p2528327@my365.dmu.ac.uk>
2021-02-04 19:51:47 +0000

    Added Simple Interface for Order system

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -4,11 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        clsOrder AnOrder = new clsOrder();
+        AnOrder.orderId = txtOrderId.Text;
+        AnOrder.dateFinalised = Convert.ToDateTime(txtDateFinalised.Text);
+        AnOrder.itemId = txtItemId.Text;
+        AnOrder.quantity = Convert.ToInt16(txtQuantity.Text);
+        AnOrder.totalPrice = Convert.ToDouble(txtTotalPrice.Text);
+        AnOrder.isEmpty = chkIsEmpty.Checked;
+        Session["AnOrder"] = AnOrder;
+        Response.Redirect("OrderViewer.aspx");
     }
 }
