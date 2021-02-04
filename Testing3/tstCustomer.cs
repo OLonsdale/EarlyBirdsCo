1bcecb37bca56c3cad4a6e8edf1b9fd0f7e4d7a7
o-maklecovs <p2528743@my365.dmu.ac.uk>
2021-02-04 00:29:22 +0000

    Added testing and class for customer management

---
+++ Testing3/tstCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Assert.IsNotNull(ACustomer);
+        }
+    }
+}
