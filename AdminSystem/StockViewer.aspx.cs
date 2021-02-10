03e0203c782a53d8e8fc05cd4ddabe79c9ff01e6
P2528833 <p2528833@my365.dmu.ac.uk>
2021-02-10 02:14:28 +0000

    Implemented a basic UI with functionality for Stock

--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -4,11 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new instance of clsStock
+        clsStock AnItem = new clsStock();
+        //receive the data from the session object
+        AnItem = (clsStock)Session["AnItem"];
+        //display the item properties for this entry 
+        Response.Write(AnItem.itemID);
+        Response.Write(AnItem.itemName);
+        Response.Write(AnItem.quantity);
+        Response.Write(AnItem.price);
+        Response.Write(AnItem.material);
+        Response.Write(AnItem.lastPurchased);
+        Response.Write(AnItem.inStock);
+       
+        
     }
 }
