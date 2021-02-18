75f4b081902e684fecff01d4b3f6bec75f70b1db
P2528618 <P2528618@GWHXX282WIN016W>
2021-02-18 17:33:20 +0000

    Updated my data entry page to add a find button, added empty javascript files in the AdminSystem folder to act as separators for different people's work. Commented out an unused variable in the customer class to stop it throwing a warning.

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -8,11 +8,12 @@ using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
 
-    }
+    //When the page loads and when the manager box is ticked.
+    protected void Page_Load(object sender, EventArgs e){}
+    protected void chkIsManager_CheckedChanged(object sender, EventArgs e){}
 
+    //When the OK button is clicked
     protected void btnOK_Click(object sender, EventArgs e)
     {
         //create instance of StaffMember class
@@ -29,9 +30,24 @@ public partial class _1_DataEntry : System.Web.UI.Page
         Response.Redirect("StaffViewer.aspx");
     }
 
-    //not sure if needed
-    protected void chkIsManager_CheckedChanged(object sender, EventArgs e)
+    protected void btnFind_Click(object sender, EventArgs e)
     {
+        //creates new instance of staff class
+        clsStaff StaffMember = new clsStaff();
+        int StaffNumber;
+        Boolean Found = false;
+        StaffNumber = Convert.ToInt32(txtStaffNumber.Text);
+        Found = StaffMember.Find(StaffNumber);
 
+        if (Found)
+        {
+            //fill in the rest of the slots
+            txtFirstName.Text = StaffMember.FirstName;
+            txtLastName.Text = StaffMember.LastName;
+            txtHourlyRate.Text = StaffMember.HourlyRate.ToString();
+            txtPhoneNumber.Text = StaffMember.PhoneNumber;
+            txtStartDate.Text = StaffMember.StartDate.ToString();
+            chkIsManager.Checked = StaffMember.IsManager;
+        }
     }
 }
