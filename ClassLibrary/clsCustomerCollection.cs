e709f8602d9501803e57015380b442229c161fe9
o-maklecovs <p2528743@my365.dmu.ac.uk>
2021-03-10 09:40:42 +0000

    Added Customer Collection class and some new tests

---
+++ ClassLibrary/clsCustomerCollection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsCustomerCollection
+    {
+        List<clsCustomer> mCustomerList = new List<clsCustomer>();
+
+        public List<clsCustomer> CustomerList {
+            get
+            {
+                return mCustomerList;
+            }
+
+            set
+            {
+                mCustomerList = value;
+            }
+        }
+        public int Count {
+            get
+            {
+                return mCustomerList.Count;
+            }
+                
+            set
+            {
+
+            }
+        }
+        public clsCustomer ThisCustomer { get; set; }
+
+        public clsCustomerCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblCustomer_SelectAll");
+
+            RecordCount = DB.Count;
+
+            while (Index < RecordCount)
+            {
+                clsCustomer ACustomer = new clsCustomer();
+
+                ACustomer.CustomerId = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerId"]);
+                ACustomer.Name = Convert.ToString(DB.DataTable.Rows[Index]["Name"]);
+                ACustomer.DateOfBirth = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateOfBirth"]);
+                ACustomer.Address = Convert.ToString(DB.DataTable.Rows[Index]["Address"]);
+                ACustomer.Email = Convert.ToString(DB.DataTable.Rows[Index]["Email"]);
+                ACustomer.IsMember = Convert.ToBoolean(DB.DataTable.Rows[Index]["IsMember"]);
+
+                mCustomerList.Add(ACustomer);
+
+                Index++;
+            }
+        }
+    }
+}
