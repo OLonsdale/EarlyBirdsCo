6486cef15b7927569f891d62b35e0af4369949ee
P2528618 <P2528618@GWHXX282WIN016W>
2021-03-14 01:39:12 +0000

    Completed week 23 work for the staff system.

--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -4,10 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack == false)
+        {
+            DisplayStaffNumbers();
+        }
+    }
+
+    void DisplayStaffNumbers()
+    {
+        clsStaffCollection StaffMembers = new clsStaffCollection();
+        lstStaffNumberList.DataSource = StaffMembers.StaffList;
+        lstStaffNumberList.DataValueField = "StaffNumber";
+        lstStaffNumberList.DataTextField  = "FirstName";
+        lstStaffNumberList.DataBind();
+    }
+
+    protected void lstStaffNumberList_SelectedIndexChanged(object sender, EventArgs e)
     {
 
     }
